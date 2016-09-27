d4e766180da833a42f4c81b9c90b08c98350d0be
darshpreetgrewal <darshpreetgrewal@aa0508-51.labs.sce.carleton.ca>
2016-09-27 12:12:55 -0400

    Changes Made

---
+++ src/AddressBook.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class AddressBook {
+
+	private List<BuddyInfo> list;
+	
+	public AddressBook(){
+		this.list=new ArrayList<BuddyInfo>();
+	}
+	
+	public void addBuddy(BuddyInfo x) {
+		if (x != null ){
+			list.add(x);
+		}
+	}
+	
+	public void removeBuddy(int x) {
+		if (x >= 0 && x <list.size() ){
+			list.remove(x);
+		}
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BuddyInfo buddy = new BuddyInfo("Darshpreet",911,18,"Carleton");
+		AddressBook AB = new AddressBook();
+		System.out.println("Hello world!");
+		AB.addBuddy(buddy);
+		AB.removeBuddy(0);
+	}
+
+}
